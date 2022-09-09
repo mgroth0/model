310fd74d5b7d54c3b2c5b17dd35226599b9c9f91
mgroth0 <mgroth49@gmail.com>
2022-09-08 20:38:20 -0400

    autocommit

---
+++ src/commonMain/kotlin/matt/model/md/md.kt
@@ -0,0 +1,5 @@
+package matt.model.md
+
+fun extractMdValue(mdText: String, key: String) = mdText.lineSequence().filter { it.startsWith("[//]: #") }
+.firstOrNull { it.substringAfter("(").substringBefore(")").substringBefore(":") == key }?.substringAfter("(")
+  ?.substringAfter(":")?.substringBefore(")")
