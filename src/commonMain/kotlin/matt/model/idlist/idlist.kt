9e84e39e8caaac42a46afced59123ffea036e197
mgroth0 <mgroth49@gmail.com>
2022-09-09 19:55:07 -0400

    autocommit

--- src/commonMain/kotlin/matt/model/idlist/idlist.kt
+++ src/commonMain/kotlin/matt/model/idlist/idlist.kt
@@ -3,11 +3,14 @@ package matt.model.idlist
 import matt.model.obj.MaybeIdentified
 
 /*because if I delete the most recent, the list still needs to remember not to reuse*/
-abstract class IdentifiedList(val list: List<MaybeIdentified<Int>>): List<MaybeIdentified<Int>> by list {
-  protected abstract var nextNewID: Int
+abstract class IdentifiedList<I: Any, O: MaybeIdentified<I>>(): List<O> {
+  protected abstract var nextNewID: I
 
-  fun createNewID(): Int {
-    println("WARNING: MUST SAVE THE IdentifiedList NOW!")
-	return nextNewID.also { nextNewID += 1 }
+  fun createNewID(): I {
+	println("WARNING: MUST SAVE THE IdentifiedList NOW!")
+	return nextNewID.also { nextNewID = increment(nextNewID) }
   }
+
+  abstract fun increment(id: I): I
+
 }
