1db155e08e44bf1a8cb4bdecd62300459590de3a
mgroth0 <mgroth49@gmail.com>
2022-10-04 18:07:05 -0400

    autocommit

--- src/jvmMain/kotlin/matt/model/latch/asyncloaded/asyncloaded.kt
+++ src/jvmMain/kotlin/matt/model/latch/asyncloaded/asyncloaded.kt
@@ -1,10 +1,42 @@
 package matt.model.latch.asyncloaded
 
 import matt.model.latch.SimpleLatch
+import java.lang.Thread.State
 
-class AsyncLoadingValue<T>() {
-  private val latch = SimpleLatch()
-  private var value: T? = null
+class DaemonLoadedValueOp<T>(name: String? = null, private val op: ()->T): Async<T>() {
+
+  private val thread by lazy {
+	Thread {
+	  value = op()
+	  latch.open()
+	  if (name != null) println("finished loading $name")
+	}.apply {
+	  isDaemon = true
+	}
+  }
+
+  @Synchronized
+  fun startLoading() {
+	require(latch.isClosed)
+	require(thread.state == State.NEW)
+	thread.start()
+  }
+}
+
+open class SyncLoadedValueOp<T>(private val op: ()->T): Async<T>() {
+
+  @Synchronized
+  fun calc(): T {
+	require(latch.isClosed)
+	value = op()
+	latch.open()
+	@Suppress("UNCHECKED_CAST")
+	return value as T
+  }
+}
+
+open class LoadedValueSlot<T>(): Async<T>() {
+
 
   @Synchronized
   fun putLoadedValue(t: T) {
@@ -12,6 +44,13 @@ class AsyncLoadingValue<T>() {
 	value = t
 	latch.open()
   }
+
+}
+
+/*is this just a future? or a modified lazy?*/
+abstract class Async<T> {
+  protected val latch = SimpleLatch()
+  protected var value: T? = null
 
   fun await(): T {
 	latch.await()
