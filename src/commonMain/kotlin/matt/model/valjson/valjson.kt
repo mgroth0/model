9c8b09e00687323591e3bc4ea734104138f45de0
mgroth0 <mgroth49@gmail.com>
2022-10-26 21:15:59 -0400

    autocommit

--- src/commonMain/kotlin/matt/model/valjson/valjson.kt
+++ src/commonMain/kotlin/matt/model/valjson/valjson.kt
@@ -1,20 +1,22 @@
 package matt.model.valjson
 
 object ValJson {
-  val WAIT_FOR_MS = 1000
-  val PORT = mapOf(
-	"task" to 65000,
-	"top" to 65001,
-	"notify" to 65002,
-	"launch" to 65003,
-	"slidespace" to 65005,
-	"brainstorm" to 65006,
-	"kjg" to 65007,
-	"pdf" to 65008,
-	"ide" to 65009,
-	"graphviz" to 65010,
-	"ktor" to 65011,
-	"spotify" to 65012,
-	"deephys:web" to 65013
-  )
+  const val WAIT_FOR_MS = 1000
+
+  object Port {
+	private var num = 65000
+	val task = num++
+	val top = num++
+	val notify = num++
+	val launch = num++
+	val slidespace = num++
+	val brainstorm = num++
+	val kjg = num++
+	val pdf = num++
+	val ide = num++
+	val graphviz = num++
+	val ktor = num++
+	val spotify = num++
+	val deephysWeb = num++
+  }
 }
