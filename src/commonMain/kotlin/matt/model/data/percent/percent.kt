2d85825ee3116c9568c60ba4256ae8f9da6c5d2c
mgroth0 <mgroth49@gmail.com>
2022-11-28 15:40:54 -0500

    autocommit

--- src/commonMain/kotlin/matt/model/data/percent/percent.kt
+++ src/commonMain/kotlin/matt/model/data/percent/percent.kt
@@ -1,5 +1,7 @@
 package matt.model.data.percent
 
+import matt.model.data.mathable.DoubleWrapper
+import matt.model.op.convert.Converter
 import kotlin.jvm.JvmInline
 
 interface PercentIdea
@@ -7,4 +9,24 @@ interface PercentIdea
 val Int.percent get() = Percent(this)
 
 @JvmInline
-value class Percent(val percent: Int): PercentIdea
+value class Percent(val percent: Double): PercentIdea, DoubleWrapper<Percent> {
+  constructor(n: Number): this(n.toDouble())
+
+  override fun fromDouble(d: Double): Percent {
+	return Percent(d)
+  }
+
+  override val asDouble: Double
+	get() = percent
+}
+
+object PercentDoubleConverter: Converter<Percent, Double> {
+  override fun convertToB(a: Percent): Double {
+	return a.asDouble
+  }
+
+  override fun convertToA(b: Double): Percent {
+	return Percent(b)
+  }
+
+}
