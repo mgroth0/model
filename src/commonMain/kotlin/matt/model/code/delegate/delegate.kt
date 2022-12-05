0b17ea9d68db2bce3e50c2ba58a2b176e56ff9bb
mgroth0 <mgroth49@gmail.com>
2022-12-05 12:29:48 -0500

    autocommit

--- src/commonMain/kotlin/matt/model/code/delegate/delegate.kt
+++ src/commonMain/kotlin/matt/model/code/delegate/delegate.kt
@@ -1,8 +1,25 @@
 package matt.model.code.delegate
 
+import kotlin.jvm.Synchronized
 import kotlin.properties.ReadOnlyProperty
+import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 class SimpleGetter<T, V>(private val o: V): ReadOnlyProperty<T, V> {
   override fun getValue(thisRef: T, property: KProperty<*>) = o
+}
+
+
+class CanOnlySetOnce<V>: ReadWriteProperty<Any?, V?> {
+  private var didSet = false
+  private var myValue: V? = null
+
+  override fun getValue(thisRef: Any?, property: KProperty<*>) = myValue
+
+  @Synchronized
+  override fun setValue(thisRef: Any?, property: KProperty<*>, value: V?) {
+	require(!didSet)
+	didSet = true
+	myValue = value
+  }
 }
