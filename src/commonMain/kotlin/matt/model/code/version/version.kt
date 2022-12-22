310702793a4fef18048d97f0d56620c6b11abf1c
mgroth0 <mgroth49@gmail.com>
2022-12-22 12:52:56 -0500

    autocommit

---
+++ src/commonMain/kotlin/matt/model/code/version/version.kt
@@ -0,0 +1,7 @@
+package matt.model.code.version
+
+import kotlin.jvm.JvmInline
+
+@JvmInline value class JavaVersion(val version: String)
+@JvmInline value class GradleVersion(val version: String)
+@JvmInline value class PythonVersion(val version: String)
