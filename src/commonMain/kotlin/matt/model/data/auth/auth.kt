2eef36bd89d738d26783954ead68e66952fa73b5
mgroth0 <mgroth49@gmail.com>
2023-02-27 12:04:09 -0500

    autocommit

--- src/commonMain/kotlin/matt/model/data/auth/auth.kt
+++ src/commonMain/kotlin/matt/model/data/auth/auth.kt
@@ -4,3 +4,11 @@ class Auth(
   val key: String,
   val secret: String
 )
+
+
+data class GitHubKeys(
+  val readPackagesForever: String,
+  val pushReleasesForever: String,
+  val apiKey: String,
+  val apiOrgKey: String
+)
