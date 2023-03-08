f11a98d77fb6e72424960efb7123a723adf87218
mgroth0 <mgroth49@gmail.com>
2023-03-08 14:03:06 -0500

    autocommit

--- src/jvmMain/kotlin/matt/model/flowlogic/latch/latch.kt
+++ src/jvmMain/kotlin/matt/model/flowlogic/latch/latch.kt
@@ -1,19 +1,39 @@
 package matt.model.flowlogic.latch
 
 import matt.model.flowlogic.await.Awaitable
+import matt.model.flowlogic.latch.LatchAwaitResult.LATCH_OPENED
+import matt.model.flowlogic.latch.LatchAwaitResult.TIMEOUT
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeoutException
 import kotlin.time.Duration
+
+enum class LatchAwaitResult {
+  LATCH_OPENED,
+  TIMEOUT
+}
 
 class SimpleLatch: Awaitable<Unit> {
 
+
   private val latch = CountDownLatch(1)
   override fun await() = latch.await()
-  fun await(timeout: Duration) = latch.await(timeout.inWholeMilliseconds, MILLISECONDS)
+  fun await(timeout: Duration): LatchAwaitResult {
+	return if (latch.await(timeout.inWholeMilliseconds, MILLISECONDS)) LATCH_OPENED
+	else TIMEOUT
+  }
+
+  fun awaitOrThrow(timeout: Duration) {
+	when (await(timeout)) {
+	  LATCH_OPENED -> Unit
+	  TIMEOUT      -> throw TimeoutException("timeout after waiting $timeout for $this")
+	}
+  }
+
   fun open() = latch.countDown()
   val isOpen get() = latch.count == 0L
   val isClosed get() = !isOpen
   fun openned() = apply {
-    open()
+	open()
   }
 }
