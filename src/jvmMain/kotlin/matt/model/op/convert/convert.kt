720cc523ce2d3be5d08da9fbaa74c230be52e1fc
mgroth0 <mgroth49@gmail.com>
2023-03-30 20:10:29 -0400

    autocommit

--- src/jvmMain/kotlin/matt/model/op/convert/convert.kt
+++ src/jvmMain/kotlin/matt/model/op/convert/convert.kt
@@ -2,28 +2,37 @@
 /*if I use classes only, don't need JvmName?*/
 package matt.model.op.convert
 
-import matt.prim.base64.encodeToBase64
-import matt.prim.base64.encodeToURLBase64
 import matt.prim.base64.decodeFromBase64
 import matt.prim.base64.decodeFromURLBase64
+import matt.prim.base64.encodeToBase64
+import matt.prim.base64.encodeToURLBase64
 
 
 @Suppress("unused")
-val RATIO_TO_PERCENT_FORMATTER = object: StringConverter<Number> {
-  override fun toString(t: Number): String {
-	return "%.3f".format(t.toDouble()*100) + "%"
-  }
+val RATIO_TO_PERCENT_FORMATTER = object : StringConverter<Number> {
+    override fun toString(t: Number): String {
+        return "%.3f".format(t.toDouble() * 100) + "%"
+    }
 
-  override fun fromString(s: String) = TODO()
+    override fun fromString(s: String) = TODO()
+}
+
+class MyRandomClass() {
+    fun myRandomFunction(): String {
+        return "hello world 4"
+    }
 }
 
 
 
-object Base64StringConverter: StringConverter<ByteArray> {
-  override fun toString(t: ByteArray) = t.encodeToBase64()
-  override fun fromString(s: String) = s.decodeFromBase64()
+
+
+object Base64StringConverter : StringConverter<ByteArray> {
+    override fun toString(t: ByteArray) = t.encodeToBase64()
+    override fun fromString(s: String) = s.decodeFromBase64()
 }
-object URLBase64StringConverter: StringConverter<ByteArray> {
-  override fun toString(t: ByteArray) = t.encodeToURLBase64()
-  override fun fromString(s: String) = s.decodeFromURLBase64()
+
+object URLBase64StringConverter : StringConverter<ByteArray> {
+    override fun toString(t: ByteArray) = t.encodeToURLBase64()
+    override fun fromString(s: String) = s.decodeFromURLBase64()
 }
