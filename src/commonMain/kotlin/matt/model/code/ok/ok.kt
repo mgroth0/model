1f75ca35e79f78d0789ef319f3db0eda66bf91ad
mgroth0 <mgroth49@gmail.com>
2023-04-01 21:09:37 -0400

    autocommit

--- src/commonMain/kotlin/matt/model/code/ok/ok.kt
+++ src/commonMain/kotlin/matt/model/code/ok/ok.kt
@@ -4,3 +4,6 @@ import kotlin.annotation.AnnotationTarget.FILE
 
 @Target(FILE) annotation class JavaIoFileIsOk
 @Target(FILE) annotation class UnnamedPackageIsOk
+
+/*Because setting JvmName is messing things up for android. Basically seems like only way to have expect and actual functions and classes for android, for now*/
+@Target(FILE) annotation class FilePackageNameMismatchIsOk
