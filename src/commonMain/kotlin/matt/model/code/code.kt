16a1bf94f68c39c41dd91217d19b2e30803c9fae
mgroth0 <mgroth49@gmail.com>
2023-04-13 17:34:42 -0400

    changed src/commonMain/kotlin/matt/model/code/code.kt

--- src/commonMain/kotlin/matt/model/code/code.kt
+++ src/commonMain/kotlin/matt/model/code/code.kt
@@ -1,9 +1,24 @@
 package matt.model.code
 
-interface Code<C: Code<C>> {
-  val code: String
-  fun formatted(): C
+interface FormatterConfig {
+    object None : FormatterConfig
 }
-interface CodeGenerator<C: Code<C>> {
-  fun generate(): C
+
+
+interface Code<C : Code<C, F>, F : FormatterConfig> {
+    val code: String
+    fun formatted(formatterConfig: F): C
 }
+
+interface SimpleFormatCode<C : SimpleFormatCode<C>> : Code<C, FormatterConfig.None> {
+    override fun formatted(formatterConfig: FormatterConfig.None): C = formatted()
+    fun formatted(): C
+}
+
+/*fun <C : Code<C, None>> C.formatted() = formatted(None)*/
+
+interface CodeGenerator<C : Code<C, *>> {
+    fun generate(): C
+}
+
+interface SimpleCodeGenerator<C : SimpleFormatCode<C>> : CodeGenerator<C>
