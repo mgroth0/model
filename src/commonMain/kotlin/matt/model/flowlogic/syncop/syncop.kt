e88f7443f76e73907e30a862d6fd62abeb065bb2
mgroth0 <mgroth49@gmail.com>
2023-04-27 15:16:26 -0400

    changed src/commonMain/kotlin/matt/model/flowlogic/syncop/syncop.kt

--- src/commonMain/kotlin/matt/model/flowlogic/syncop/syncop.kt
+++ src/commonMain/kotlin/matt/model/flowlogic/syncop/syncop.kt
@@ -8,36 +8,37 @@ import matt.lang.sync.inSync
 
 
 class AntiDeadlockSynchronizer {
-  companion object {
-	val yesIUseLang = YesIUseLang
-  }
+    companion object {
+        val yesIUseLang = YesIUseLang
+    }
 
-  private var currentWorkerCount = 0
+    private var currentWorkerCount = 0
 
 
-  @NullToReduceObjects
-  private var opQueue: MutableList<Op>? = null
+    @NullToReduceObjects
+    private var opQueue: MutableList<Op>? = null
 
-  fun useInternalData(op: Op) {
-	inSync(this) {
-	  currentWorkerCount += 1
-	}
-	op()
-	inSync(this) {
-	  currentWorkerCount -= 1
-	  if (currentWorkerCount == 0) {
-		opQueue?.forEach { it() }
-		opQueue = null
-	  }
-	}
-  }
+    fun useInternalData(op: Op) {
+        inSync(this) {
+            currentWorkerCount += 1
+        }
+        op()
+        inSync(this) {
+            currentWorkerCount -= 1
+            if (currentWorkerCount == 0) {
+                opQueue?.forEach { it() }
+                opQueue = null
+            }
+        }
+    }
 
-  @OnlySynchronizedOnJvm fun operateOnInternalDataNowOrLater(op: Op) {
-	if (currentWorkerCount > 0) {
-	  (opQueue ?: mutableListOf<Op>().also {
-		opQueue = it
-	  }) += op
-	} else op()
-  }
+    @OnlySynchronizedOnJvm
+    fun operateOnInternalDataNowOrLater(op: Op) {
+        if (currentWorkerCount > 0) {
+            (opQueue ?: mutableListOf<Op>().also {
+                opQueue = it
+            }) += op
+        } else op()
+    }
 
 }
