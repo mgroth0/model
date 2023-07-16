0e79c94b127adc7e8c4d8344fb55db592f715595
mgroth0 <mgroth49@gmail.com>
2023-07-16 19:00:23 -0400

    changed src/commonMain/kotlin/matt/model/data/prop/prop.kt

--- src/commonMain/kotlin/matt/model/data/prop/prop.kt
+++ src/commonMain/kotlin/matt/model/data/prop/prop.kt
@@ -4,3 +4,12 @@ interface SimpleSuspendProperty<T : Any> {
     suspend fun get(): T?
     suspend fun set(value: T?)
 }
+
+suspend fun <T : Any> SimpleSuspendProperty<T>.unsynchronizedGetOrPut(op: () -> T): T {
+    val t = get()
+    return if (t == null) {
+        val r = op()
+        set(r)
+        r
+    } else t
+}
