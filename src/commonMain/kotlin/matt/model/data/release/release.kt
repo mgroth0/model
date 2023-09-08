5507c5aead29c1fad629f011d166d20e13666c12
mgroth0 <mgroth49@gmail.com>
2023-09-07 22:14:37 -0400

    changed src/commonMain/kotlin/matt/model/data/release/release.kt

--- src/commonMain/kotlin/matt/model/data/release/release.kt
+++ src/commonMain/kotlin/matt/model/data/release/release.kt
@@ -8,56 +8,66 @@ import matt.model.data.release.UpdateLevel.PUBLISH
 
 @Serializable
 data class Release(
-  val name: String,
-  val zipball_url: String,
-  val tarball_url: String,
-  val commit: Commit,
-  val node_id: String
+    val name: String,
+    val zipball_url: String,
+    val tarball_url: String,
+    val commit: Commit,
+    val node_id: String
 ) {
-  val version by lazy { Version(name) }
-  override fun toString() = "a ${Release::class.simpleName} with version $version"
+    val version by lazy { Version(name) }
+    override fun toString() = "a ${Release::class.simpleName} with version $version"
 }
 
 @Serializable
 class Commit(
-  /*sha must be private because it is not correctly retrieved from authenticated github client*/
-  private val sha: String,
-  /*url must be private because I DONT KNOW if it is correctly retrieved from authenticated github client*/
-  private val url: String
+    /*sha must be private because it is not correctly retrieved from authenticated github client*/
+    private val sha: String,
+    /*url must be private because I DONT KNOW if it is correctly retrieved from authenticated github client*/
+    private val url: String
 )
 
-@Serializable data class VersionInfo(
+@Serializable
+data class VersionInfo(
     val version: Version,
     val downloadURL: String
 )
 
 @Serializable
-data class Version(val first: Int, val second: Int, val third: Int): Comparable<Version> {
-  constructor(versionString: String): this(
-	versionString.split(".")[0].toInt(), versionString.split(".")[1].toInt(), versionString.split(".")[2].toInt()
-  )
+data class Version(
+    val first: Int,
+    val second: Int,
+    val third: Int
+) : Comparable<Version> {
 
-  override operator fun compareTo(other: Version): Int {
-	return (first.compareTo(other.first))
-			 .takeIf { it != 0 } ?: ((second.compareTo(other.second)).takeIf { it != 0 }
-									 ?: (third.compareTo(other.third)))
-  }
 
-  override fun toString(): String {
-	return listOf(first, second, third).joinToString(".")
-  }
+    constructor(versionString: String) : this(
+        versionString.split(".")[0].toInt(), versionString.split(".")[1].toInt(), versionString.split(".")[2].toInt()
+    )
 
-  fun increment(level: UpdateLevel): Version {
-	return when (level) {
-	  PUBLISH -> Version(first + 1, 0, 0)
-	  FEATURE -> Version(first, second + 1, 0)
-	  PATCH   -> Version(first, second, third + 1)
-	}
-  }
+
+
+    override operator fun compareTo(other: Version): Int {
+        return (first.compareTo(other.first))
+            .takeIf { it != 0 } ?: ((second.compareTo(other.second)).takeIf { it != 0 }
+            ?: (third.compareTo(other.third)))
+    }
+
+    override fun toString(): String {
+        return listOf(first, second, third).joinToString(".")
+    }
+
+    fun increment(level: UpdateLevel): Version {
+        return when (level) {
+            PUBLISH -> Version(first + 1, 0, 0)
+            FEATURE -> Version(first, second + 1, 0)
+            PATCH   -> Version(first, second, third + 1)
+        }
+    }
 
 }
 
 @Serializable
 enum class UpdateLevel {
-  PUBLISH, FEATURE, PATCH
+    PUBLISH, FEATURE, PATCH;
+    val firstVersion get() = Version(0,0,0).increment(this)
 }
